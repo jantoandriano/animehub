b6e91acf85af4383552a4dac97550c230a2da36b
jantoandriano <motulojanto@gmail.com>
2024-05-15 09:02:36 +0700

    Author: Janto Date: 15 Mei 2024
    - Create Component AnimeList

---
+++ app/components/list/AnimeList.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import List from "./List";
+
+type Props = {
+    q: string;
+    currentPage: string;
+}
+
+async function getData(q: string, currentPage: string) {    
+    const params = `?limit=10&q=${q}&page=${currentPage}`
+    const res = await fetch(`https://api.jikan.moe/v4/anime${params}`)
+    const data = await res.json();
+
+    if (!res.ok) {
+        throw new Error('Failed to fetch data')
+    }
+
+    return data
+}
+
+
+const AnimeList: React.FC<Props> = async ({ q, currentPage }) => {
+    const data = await getData(q, currentPage)
+    return (
+        <div>
+            <List heading="Home" data={data.data} />
+        </div>
+    )
+}
+
+export default AnimeList
