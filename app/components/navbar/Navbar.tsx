94cfabe4c7c7de9839cf0902d9831d1219bbe190
jantoandriano <motulojanto@gmail.com>
2024-05-16 14:50:00 +0700

    Author: Janto Date: 16 Mei 2024
    - Update navbar component

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -9,25 +9,11 @@ const Navbar = () => {
                 <Image src="/logo.png" className="h-8" alt="website-logo" width={70} height={50} />
                 <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Anime Hub</span>
             </Link>
-            <button data-collapse-toggle="navbar-default" type="button" className="navbar-btn-menu" aria-controls="navbar-default" aria-expanded="false">
-                <span className="sr-only">Open main menu</span>
-                <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
-                </svg>
-            </button>
+            
             <div className="hidden w-full md:block md:w-auto" id="navbar-default">
                 <ul className="navbar-link-container">
                     <li>
                         <Link href="/" className="navbar-link" aria-current="page">Home</Link>
-                    </li>
-                    <li>
-                        <Link href="/" className="navbar-link" aria-current="page">About</Link>
-                    </li>
-                    <li>
-                        <Link href="/" className="navbar-link" aria-current="page">Team</Link>
-                    </li>
-                    <li>
-                        <Link href="#" className="navbar-link">Contact</Link>
                     </li>
                 </ul>
             </div>
